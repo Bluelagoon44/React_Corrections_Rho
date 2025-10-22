441e638af4b07b3445589a039181ab05ae7cc8d8
Brendan <brendan.viale@gmail.com>
2025-10-22 10:45:48 +0200

    Ajout alias

--- src/TD16/TD16.jsx
+++ src/TD16/TD16.jsx
@@ -1,9 +1,11 @@
+import Button from "@components/Button/Button"
 import useToggle from "../hooks/useToggle"
 
 const TD16 = () => {
     const [isOpen, handleIsOpen] = useToggle()
     return (
         <div>
+            <Button label="aze" />
             <button onClick={handleIsOpen}>Yolo {isOpen ? "fermer" : "ouvrir"}</button>
             {isOpen && <p>Yolo ouvert !</p>}
         </div>
